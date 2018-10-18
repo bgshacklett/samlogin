9a70b697ff8c413ed823857860afafe469e88d76
Brian Shacklett <brian@digital-traffic.net>
2018-10-18 08:55:18 -0400

    build(gulp): add Gulp to streamline releases

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,60 @@
-const { parallel, src, task } = require('gulp');
-const eslint = require('gulp-eslint');
+const {
+  src,
+  dest,
+  parallel,
+  series,
+  task,
+} = require('gulp');
+
+const eslint    = require('gulp-eslint');
+const changeLog = require('gulp-conventional-changelog');
+const bump      = require('gulp-bump');
+const log       = require('gulplog');
+const minimist  = require('minimist');
+
+
+/*
+ * Begin Task Definitions
+ * */
+task(
+  'bump-version',
+  () => {
+    const opts = minimist(process.argv.slice(2), { string: 'type' });
+
+    return src(['./package.json'])
+          .pipe(bump({ type: opts.type }).on('error', log.error))
+          .pipe(dest('./'));
+  }
+  ,
+);
+
+task(
+  'changelog',
+  () => src('CHANGELOG.md', { buffer: false })
+        .pipe(changeLog({ preset: 'angular' }))
+        .pipe(dest('./'))
+  ,
+);
+
 
 task(
   'lint',
   () => src([
-    '**/*.js',
-    '!node_modules/**',
-  ])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError())
+              '**/*.js',
+              '!node_modules/**',
+            ])  // eslint-disable-line indent
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError())
+  ,
+);
+
+task(
+  'release',
+  series(
+    'bump-version',
+    'changelog',
+  )
   ,
 );
 
@@ -17,5 +62,6 @@ task(
   'default',
   parallel(
     'lint',
-  ),
+  )
+  ,
 );
